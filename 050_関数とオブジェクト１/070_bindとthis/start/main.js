ce142eec217224e04ca04b6f756d9c596c62af91
Yuto299 <ota.se.ke@outlook.jp>
2025-03-28 15:41:56 +0900

    bindとthis

--- 050_関数とオブジェクト１/070_bindとthis/start/main.js
+++ 050_関数とオブジェクト１/070_bindとthis/start/main.js
@@ -1,15 +1,29 @@
 window.name = 'John';
 
 const person = {
-    name: 'Tom',
-    hello: function() {
-        console.log('Hello ' + this.name);
-    }
-}
+  name: 'Tom',
+  hello: function () {
+    console.log('Hello ' + this.name);
+  },
+};
 person.hello();
 
+const helloTom = person.hello.bind(person);
+
 function fn(ref) {
-    ref();
+  ref();
 }
 
-fn(person.hello);
+fn(helloTom);
+
+function a() {
+  console.log('Hello ' + this.name);
+}
+
+const b = a.bind({ name: 'Tim' });
+
+b();
+
+//bindメソッドは、関数に対して呼び出し元のオブジェクトを指定することができる
+//bindメソッドで呼び出し元のオブジェクトを指定すると、そのオブジェクトがthisとして設定される
+//bindメソッドで呼び出し元のオブジェクトを指定した関数を他の関数に渡しても、thisは設定されない
