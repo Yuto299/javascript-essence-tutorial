b2c0237150f17d57328d3eb2c9c59d46d30ec893
Yuto299 <ota.se.ke@outlook.jp>
2025-03-28 15:55:56 +0900

    callとapply

--- 050_関数とオブジェクト１/080_call,applyとthis/start/main.js
+++ 050_関数とオブジェクト１/080_call,applyとthis/start/main.js
@@ -1,7 +1,19 @@
-function a() {
-  console.log("hello " + this.name);
+function a(name) {
+  console.log('hello ' + name);
 }
 
-const b = a.bind({ name: "Tim" });
+const tim = { name: 'Tim' };
 
+const b = a.bind(tim);
 b();
+//めんどくさい
+
+a.apply(tim, ['Tim']); //配列
+a.call(tim, 'Tim'); //文字列
+//簡単、関数の実行まで行われる
+
+const array = [1, 2, 3, 4, 5];
+
+// const result = Math.max.apply(null, array);
+const result = Math.max(...array);
+console.log(result);
