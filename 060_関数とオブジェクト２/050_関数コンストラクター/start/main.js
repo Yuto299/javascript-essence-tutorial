fc5cd700521d192bdc384e5b24e5f9facd317144
Yuto299 <ota.se.ke@outlook.jp>
2025-04-03 00:16:23 +0900

    fn関数を追加し、クロージャを使用して変数dをスコープ内で管理。fn1関数を定義し、引数に基づいて計算を行う。fn2関数を追加。

---
+++ 060_関数とオブジェクト２/050_関数コンストラクター/start/main.js
@@ -0,0 +1,14 @@
+let d = 0;
+function fn() {
+  let d = 1;
+  const fn1 = new Function('a', 'b', 'return a + b * d');
+  return fn1;
+}
+
+const f = fn();
+const result = f(1, 2);
+console.log(result);
+
+function fn2(a, b) {
+  return a + b;
+}
