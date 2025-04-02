1f24df44a77cdfbf501d48d5e48eb2cce467fb5a
Yuto299 <ota.se.ke@outlook.jp>
2025-04-03 00:08:38 +0900

    F関数の戻り値をオブジェクトに変更し、インスタンスの確認を追加。fn関数を実装し、引数に応じた処理を追加。（instanceof）

--- 060_関数とオブジェクト２/040_instanceof/start/main.js
+++ 060_関数とオブジェクト２/040_instanceof/start/main.js
@@ -1,7 +1,22 @@
 function F(a, b) {
-    this.a = a;
-    this.b = b;
-    // return {a: 1};
+  this.a = a;
+  this.b = b;
+  return { a: 1 };
 }
 
-F.prototype.c = function() {}
+F.prototype.c = function () {};
+
+const instance = new F(1, 2);
+console.log(instance instanceof F); //falseだとFから生成されたものではない
+console.log(instance._proto__ === F.prototype);
+
+function fn(arg) {
+  if (arg instanceof Array) {
+    arg.push('value');
+  } else {
+    arg['key'] = 'value';
+  }
+  console.log(arg);
+}
+
+fn([]);
