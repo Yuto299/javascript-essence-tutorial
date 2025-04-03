8a55ede0f0aede97aff2b07f2e2b7958c5bc0eae
Yuto299 <ota.se.ke@outlook.jp>
2025-04-03 23:58:04 +0900

    プロトタイプ継承

--- 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
+++ 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
@@ -3,6 +3,22 @@ function Person(name, age) {
   this.age = age;
 }
 
-Person.prototype.hello = function() {
+Person.prototype.hello = function () {
   console.log('hello ' + this.name);
+};
+
+function Japanese(name, age) {
+  Person.call(this, name, age);
 }
+
+Japanese.prototype = Object.create(Person.prototype);
+//プロトタイプ継承
+
+Japanese.prototype.hello = function () {
+  console.log('konnichiwa ' + this.name);
+};
+
+const taro = new Japanese('Taro', 22);
+console.log(taro);
+
+taro.hello();
